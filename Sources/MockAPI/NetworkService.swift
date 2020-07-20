497f892ba68852e2b3cd91ea179afbc48adc7f86
Hiroya Hinomori <nexearth@gmail.com>
2020-07-21 00:21:52 +0900

    tweaks

--- Sources/MockAPI/NetworkService.swift
+++ Sources/MockAPI/NetworkService.swift
@@ -105,98 +105,102 @@ public class StubNetworkService: NetworkServiceType {
     }
 }
 
-private class OAuthHandler: RequestInterceptor {
-    private typealias RefreshCompletion = (_ succeeded: Bool, _ accessToken: String?, _ refreshToken: String?) -> Void
+public class NetworkService: NetworkServiceType {
+    class OAuthHandler: RequestInterceptor {
+        private var isRefreshing = false
+        private var requestsToRetry: [(RetryResult) -> Void] = []
 
-    private var isRefreshing = false
-    private var requestsToRetry: [(RetryResult) -> Void] = []
+        private let lock = NSLock()
+        private var accessTokenRepository: TokenRepositoryType
+        private var refreshTokenHandler: RefreshTokenHandler?
 
-    private let lock = NSLock()
-    private var accessTokenRepository: TokenRepositoryType?
-    private var refreshTokenHandler: RefreshTokenHandler?
+        public init(accessTokenRepository: TokenRepositoryType, refreshTokenHandler: RefreshTokenHandler? = nil) {
+            self.accessTokenRepository = accessTokenRepository
+            self.refreshTokenHandler = refreshTokenHandler
+        }
 
-    init(accessTokenRepository: TokenRepositoryType?,
-         refreshTokenHandler: RefreshTokenHandler? = nil) {
-        self.accessTokenRepository = accessTokenRepository
-        self.refreshTokenHandler = refreshTokenHandler
-    }
-
-    func adapt(_ urlRequest: URLRequest, for _: Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
-        if let accessToken = accessTokenRepository?.token {
-            var urlRequest = urlRequest
-            urlRequest.setValue("Bearer " + accessToken.rawValue, forHTTPHeaderField: "Authorization")
+        func adapt(_ urlRequest: URLRequest, for _: Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
+            if let accessToken = accessTokenRepository.token {
+                var urlRequest = urlRequest
+                urlRequest.setValue("Bearer " + accessToken.rawValue, forHTTPHeaderField: "Authorization")
+                return completion(.success(urlRequest))
+            }
             return completion(.success(urlRequest))
         }
-        return completion(.success(urlRequest))
-    }
 
-    func retry(_ request: Request, for _: Session, dueTo _: Error, completion: @escaping (RetryResult) -> Void) {
-        defer {
-            lock.unlock()
+        func retry(_ request: Request, for _: Session, dueTo _: Error, completion: @escaping (RetryResult) -> Void) {
+            defer {
+                lock.unlock()
+            }
+
+            lock.lock()
+            if let response = request.task?.response as? HTTPURLResponse, response.statusCode == 401 {
+                requestsToRetry.append(completion)
+
+                if !isRefreshing {
+                    refreshTokens { [weak self] json in
+                        guard let strongSelf = self else {
+                            return
+                        }
+
+                        strongSelf.lock.lock()
+                        defer {
+                            strongSelf.lock.unlock()
+                        }
+                        if let strongJson = json {
+                            strongSelf.refreshTokenHandler?.completion(strongJson)
+                        }
+                        strongSelf.requestsToRetry.forEach { $0(.doNotRetry) }
+                        strongSelf.requestsToRetry.removeAll()
+                    }
+                }
+            } else {
+                completion(.doNotRetry)
+            }
         }
 
-        lock.lock()
-        if let response = request.task?.response as? HTTPURLResponse, response.statusCode == 401 {
-            requestsToRetry.append(completion)
+        func refreshTokens(completion: @escaping ([String: Any]?) -> Void) {
+            guard !isRefreshing, let target = refreshTokenHandler?.endpointTarget else {
+                return
+            }
 
-            if !isRefreshing {
-                refreshTokens { [weak self] json in
+            isRefreshing = true
+
+            var endpoint = MoyaProvider.defaultEndpointMapping(for: target)
+            if let refreshToken = refreshTokenHandler?.tokenRepository.token {
+                endpoint = endpoint.adding(newHTTPHeaderFields: ["Authorization": "Bearer \(refreshToken.rawValue)"])
+            }
+
+            let request = try! endpoint.urlRequest() // swiftlint:disable:this force_try
+            Session.default.request(request)
+                .responseJSON { [weak self] response in
                     guard let strongSelf = self else {
                         return
                     }
-
-                    strongSelf.lock.lock()
-                    defer {
-                        strongSelf.lock.unlock()
+                    if case let .success(value) = response.result,
+                        let json = value as? [String: Any] {
+                        completion(json)
+                    } else {
+                        completion(nil)
                     }
-                    if let strongJson = json {
-                        strongSelf.refreshTokenHandler?.completion(strongJson)
-                    }
-                    strongSelf.requestsToRetry.forEach { $0(.doNotRetry) }
-                    strongSelf.requestsToRetry.removeAll()
+                    strongSelf.isRefreshing = false
                 }
-            }
-        } else {
-            completion(.doNotRetry)
         }
     }
 
-    private func refreshTokens(completion: @escaping ([String: Any]?) -> Void) {
-        guard !isRefreshing, let target = refreshTokenHandler?.endpointTarget else {
-            return
-        }
-
-        isRefreshing = true
-
-        var endpoint = MoyaProvider.defaultEndpointMapping(for: target)
-        if let refreshToken = refreshTokenHandler?.tokenRepository.token {
-            endpoint = endpoint.adding(newHTTPHeaderFields: ["Authorization": "Bearer \(refreshToken.rawValue)"])
-        }
-
-        let request = try! endpoint.urlRequest() // swiftlint:disable:this force_try
-        Session.default.request(request)
-            .responseJSON { [weak self] response in
-                guard let strongSelf = self else {
-                    return
-                }
-                if case let .success(value) = response.result,
-                    let json = value as? [String: Any] {
-                    completion(json)
-                } else {
-                    completion(nil)
-                }
-                strongSelf.isRefreshing = false
-            }
-    }
-}
-
-public class NetworkService: NetworkServiceType {
     private let provider: MoyaProvider<AbstractTarget>
 
-    public init(accessTokenRepository: TokenRepositoryType?,
-                refreshTokenHandler: RefreshTokenHandler? = nil) {
-        let handler = OAuthHandler(accessTokenRepository: accessTokenRepository,
-                                   refreshTokenHandler: refreshTokenHandler)
+    public init() {
+        let provider = MoyaProvider<AbstractTarget>(
+            plugins: [
+                loggingPlugin(),
+            ]
+        )
+        self.provider = provider
+    }
+
+    public init(accessTokenRepository: TokenRepositoryType, refreshTokenHandler: RefreshTokenHandler? = nil) {
+        let handler = OAuthHandler(accessTokenRepository: accessTokenRepository, refreshTokenHandler: refreshTokenHandler)
         let provider = MoyaProvider<AbstractTarget>(
             session: .init(interceptor: handler),
             plugins: [
